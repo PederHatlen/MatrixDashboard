6b88cc8d5e7a0576e02ccb9a8c250052ad9d7698
Peder <pederhatlen@gmail.com>
2024-11-22 12:05:36 +0100

    New menu system

--- pannels/__init__.py
+++ pannels/__init__.py
@@ -1,10 +1,18 @@
-import pkgutil
+import pkgutil, os
 
+currentPath = os.path.dirname(os.path.abspath(__file__))
+
+dirs = [d for d in list(os.walk("./pannels/"))[0][1] if "__" not in d]
+
+menu = {}
 packages = []
-
 __all__ = []
-for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    __all__.append(module_name)
-    _module = loader.find_module(module_name).load_module(module_name)
-    globals()[module_name] = _module
-    packages.append(_module)
+for d in dirs:
+    for loader, module_name, is_pkg in pkgutil.walk_packages([f"{currentPath}/{d}"]):
+        _module = loader.find_module(module_name).load_module(module_name)
+        globals()[module_name] = _module
+        packages.append(_module)
+        __all__.append(module_name)
+        menu[module_name] = d
+
+print(f"Pannels that where loaded: {__all__}")
