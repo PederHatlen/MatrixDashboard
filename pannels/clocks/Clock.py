d895cd0078fa022cd45803673685801d82b0ff0e
Peder <pederhatlen@gmail.com>
2025-01-19 14:11:33 +0100

    Locale

--- pannels/clocks/Clock.py
+++ pannels/clocks/Clock.py
@@ -1,6 +1,15 @@
-import datetime, math
+import datetime, math, locale
 from PIL import Image, ImageDraw
 from functions import *
+from contextlib import contextmanager
+
+LOCALE_STRING = 'nb_NO.utf8' # Change to your own (trust me it's the easiest way)
+
+@contextmanager
+def setlocale(name):
+    saved = locale.setlocale(locale.LC_ALL)
+    try: yield locale.setlocale(locale.LC_ALL, name)
+    finally: locale.setlocale(locale.LC_ALL, saved)
 
 small05 = font["small05"]
 
@@ -26,18 +35,18 @@ def get(ts):
         x, y = math.cos(math.radians(deg)), math.sin(math.radians(deg))
         d.line((round(x*(r-w+2)) + o, round(y*(r-w+2)) + o, round(x*r)+o, round(y*r)+o), color["orange"], 1)
 
-    hour = ((now.hour/12) * 2*math.pi - math.pi/2)
-    minute = ((now.minute/60) * 2*math.pi - math.pi/2)
-    second = ((now.second/60) * 2*math.pi - math.pi/2)
+    hour = (now.hour/12) * 2*math.pi - math.pi/2
+    minute = (now.minute/60) * 2*math.pi - math.pi/2
+    second = (now.second/60) * 2*math.pi - math.pi/2
 
-    d.line((r, r, round(math.cos(hour)*hLen)+o, round(math.sin(hour)*hLen)+o), color["white"], 1)
-    d.line((r, r, round(math.cos(minute)*mLen)+o, round(math.sin(minute)*mLen)+o), color["white"], 1)
-    d.line((r, r, round(math.cos(second)*sLen)+o, round(math.sin(second)*sLen)+o), color["orange"])
+    d.line((o, o, round(math.cos(hour)*hLen)+o, round(math.sin(hour)*hLen)+o), color["white"], 1)
+    d.line((o, o, round(math.cos(minute)*mLen)+o, round(math.sin(minute)*mLen)+o), color["white"], 1)
+    d.line((o, o, round(math.cos(second)*sLen)+o, round(math.sin(second)*sLen)+o), color["orange"])
 
-
-    d.text((34, 0), now.strftime("%A"), color["orange"], small05)
-    d.text((34, 8), now.strftime("%-d %b"), color["white"], small05)
-    d.text((34, 16), now.strftime("Week %W"), color["white"], small05)
+    with setlocale(LOCALE_STRING):
+        d.text((34, 0), now.strftime("%A"), color["orange"], small05)
+        d.text((34, 8), now.strftime("%-d %b")[:-1], color["white"], small05)
+        d.text((34, 16), f"Uke {str(now.isocalendar()[1]).rjust(2, '0')}", color["white"], small05)
 
     # d.point((15, 15), color["orange"])
 
